d516bbf8c88a85bc9717b9233f25b73c15aa776b
Colin Newell <colin.newell@gmail.com>
2016-03-15 18:51:20 +0000

    Started to integrate sqlalchemy

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
+#db = SQLAlchemy(app)
+#
+#from models import Employee
 
 
 @app.route('/')
--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'j1etLfY9D6ioCeJsUyXfNLguvB04O4bj'
+    DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
