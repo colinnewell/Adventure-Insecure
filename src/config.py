26e0afdbc84393e6eff5b0753857d57d67349ddc
Colin Newell <colin.newell@gmail.com>
2016-03-21 21:02:49 +0000

    Turn off SQLalchemy setting

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'j1etLfY9D6ioCeJsUyXfNLguvB04O4bj'
     DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
