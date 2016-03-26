caa80c07e9ac40b96b834a3663da0331dd69d10f
Colin Newell <colin.newell@gmail.com>
2016-03-26 20:41:33 +0000

    Removed redundant line

--- src/manage.py
+++ src/manage.py
@@ -5,8 +5,6 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db
 
 
-app.config.from_object(os.environ['APP_SETTINGS'])
-
 migrate = Migrate(app, db)
 manager = Manager(app)
 
