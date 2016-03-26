32d9900d006e9ba4aba5603b489c9bcbb754d2a0
Colin Newell <colin.newell@gmail.com>
2016-03-26 21:32:53 +0000

    Added tests

--- src/app/auth/forms.py
+++ src/app/auth/forms.py
@@ -1,10 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Required, Email, EqualTo
 
 
 class LoginForm(Form):
-    email = TextField('Email Address', [Email(),
+    email = StringField('Email Address', [Email(),
                       Required(message='This is required')])
     password = PasswordField('Password',
                              [Required(message='Must specify a password')])
---
+++ src/test_app.py
@@ -0,0 +1,49 @@
+import os
+os.environ['APP_SETTINGS'] = 'config.TestingConfig'
+from app import app, db
+import unittest
+import tempfile
+import re
+
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, self.db_filename = tempfile.mkstemp()
+        # FIXME: this isn't actually working.
+        app.config['DATABASE_URI'] = 'sqlite:///' + self.db_filename
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(self.db_filename)
+
+    def test_base(self):
+        rv = self.app.get('/')
+        assert 'Hello' in rv.data.decode('utf-8')
+
+    def get_csrf_token(self, rv):
+        m = re.search('.*name="csrf_token".*?value="([^"]+)".*',
+                      rv.data.decode('utf-8'))
+        csrf = m.group(1)
+        return csrf
+
+    def login(self, user, password):
+        rv = self.app.get('/auth/login')
+        csrf = self.get_csrf_token(rv)
+        return self.app.post('/auth/login', data=dict(
+            email=user,
+            password=password,
+            csrf_token=csrf,
+        ), follow_redirects=True)
+
+    def test_login_fail(self):
+        rv = self.login('bogus', 'test')
+        assert 'Invalid email' in rv.data.decode('utf-8')
+        rv = self.login('bogus@example.org', 'test')
+        assert 'Incorrect email or password' in rv.data.decode('utf-8')
+
+
+if __name__ == '__main__':
+    unittest.main()
