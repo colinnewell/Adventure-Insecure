f3006d68bdbbab373f80dc299265e6b736c3d702
Colin Newell <colin.newell@gmail.com>
2016-07-16 17:16:57 +0100

    Started to try to setup ldap

---
+++ docker/ldap_init.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from ldap3 import Server, Connection, ALL
+
+server = Server('server_name')
+
---
+++ docker/setup_ldap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+IP=`docker inspect --format '{{ range .NetworkSettings.Networks}}{{ .IPAddress }}{{end}}' compose_ldap_1`
+echo ldapadd -x -h $IP -D cn=admin,dc=adventure,dc=org -w notaverysecurepassword -f initial_schema.ldif
+ldapadd -x -h $IP -D cn=admin,dc=adventure,dc=org -w notaverysecurepassword -f initial_schema.ldif
+echo installed posix schemas.
+echo ldapsearch -x -h $IP -b dc=adventure,dc=org -D "cn=admin,dc=adventure,dc=org" -w devpassword
+ldapsearch -x -h $IP -b dc=adventure,dc=org -D "cn=admin,dc=adventure,dc=org" -w notaverysecurepassword
