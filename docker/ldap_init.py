98faeab04e1932a93d9dbe2fd0a66e9f4639ff89
Colin Newell <colin.newell@gmail.com>
2016-07-21 22:01:02 +0100

    Started to add sudo support

--- docker/ldap_init.py
+++ docker/ldap_init.py
@@ -16,6 +16,15 @@ class LDAP:
             'gidNumber': id_num,
         })
 
+    def add_sudo_group(self, group):
+        self.connection.add('cn=%s,ou=SUDOers,%s' % (group, self.dn), 
+                ['top', 'sudoRole'], {
+            'cn': group,
+            'sudoUser': group,
+            'sudoHost': 'ALL',
+            'sudoCommand': 'ALL',
+        })
+
     def add_user(self, username, name, surname, password, group_id, user_id):
         # escape usernames
         # FIXME: hash password.
@@ -41,4 +50,5 @@ ldap = LDAP(conn, 'dc=adventure,dc=org')
 
 ldap.add_group('developers', 5000)
 ldap.add_user('dev1', 'Developer 1', '1', 'insecure', 5000, 10000)
+ldap.add_sudo_group('%developers')
 
