c5d151f0f21d78ca729f8667297cb3231849e5e5
Colin Newell <colin.newell@gmail.com>
2016-08-06 22:41:30 +0100

    Check for admin user

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,5 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from app.csrf import generate_csrf, validate_csrf
@@ -15,6 +15,7 @@ flask_wtf.csrf.generate_csrf = generate_csrf
 flask_wtf.csrf.CsrfProtect(app)
 
 db = SQLAlchemy(app)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -33,3 +34,20 @@ app.register_blueprint(lunch)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# FIXME: should move this out of the core app code.
+from app.auth.models import User
+from flask import session
+def _get_user():
+    if 'user_id' in session:
+        try:
+            u = User.query.filter_by(id=session['user_id']).one()
+            return u
+        except:
+            pass
+    return None
+
+@app.context_processor
+def inject_user():
+    return dict(user=_get_user())
+    
