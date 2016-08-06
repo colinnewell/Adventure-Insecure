eb6534890a4c4c1177d6d966501835e9d8fc9919
Colin Newell <colin.newell@gmail.com>
2016-08-06 21:46:54 +0100

    Added an admin flag

--- src/app/auth/models.py
+++ src/app/auth/models.py
@@ -1,6 +1,7 @@
 from app import db
 from app.security import secure_token
 from app.base_model import Base
+from sqlalchemy.sql import text
 
 
 class SignupAttempt(Base):
@@ -31,14 +32,16 @@ class User(Base):
 
     email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(192), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, server_default=text('false'))
 
     # FIXME: should allow deactivation of users.
 
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, admin=False):
 
         self.name = name
         self.email = email
         self.password = password
+        self.admin = admin
 
     def __repr__(self):
         return "<User %r, %r>" % (self.name, self.email)
